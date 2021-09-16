8c355e7847ba6e773c6998fb522e039e1cdfe3e7
Camilo Castellanos <ext_cacastel@mercadolibre.com.co>
2021-09-16 16:22:22 -0500

    update

--- index.js
+++ index.js
@@ -2,13 +2,20 @@
 
 const http = require('http')
 const bodyParser = require('./lib/bodyParser')
+const cors = require('./lib/cors')
 const router = require('./lib/router')
 
 const app = router()
 
 const users = []
 
-app.get('/users', (request, response, _) => {
+app.get('/users/:id/detail', (request, response) => {
+    response.json({
+        id: request.params.id
+    })
+})
+
+app.get('/users', cors(), (request, response, _) => {
     response
         .json({
             users
